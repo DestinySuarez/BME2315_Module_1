7e46f00b73adc47d25e9c738b1956c3bfdd9a3ab
DestinySuarez <ukr7sy@virginia.edu>
2026-09-10 10:37:41 -0400

    9/10
    Changes we did in class

---
+++ Main_Destiny.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+df = pd.read_csv("/Users/destinysuarez/Desktop/Computational BME/Module 1/Metadata and Protein Data for Module 1.csv")
+
+for header in df.columns:
+    print(header)
